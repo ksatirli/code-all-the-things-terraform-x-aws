9afcc63572716092b3dc759a335ed0a3867fd207
Kerim Satirli <k.satirli@gmail.com>
2020-06-05 18:31:08 +0200

    adds key to EC2

--- instance.tf
+++ instance.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "hack_the_ne" {
   instance_type     = var.instance_type
   availability_zone = var.availability_zone
 
+  key_name = aws_key_pair.hack_the_ne.key_name
   tags = {
     Name = "Terraform-managed EC2 Instance for Hack the NE"
   }
 }
-
