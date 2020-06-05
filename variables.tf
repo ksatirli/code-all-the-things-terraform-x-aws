80a759d5d41f088f0851d0a0c504914afc6aa98b
Kerim Satirli <k.satirli@gmail.com>
2020-06-05 21:36:44 +0200

    adds resource for S3 Website Bucket

--- variables.tf
+++ variables.tf
@@ -28,3 +28,38 @@ data "http" "icanhazip" {
   // this value will be available in data.http.icanhazip.body
   url = "https://icanhazip.com/"
 }
+
+variable "website_bucket_name" {
+  type        = string
+  description = "S3 Bucket Name for Website"
+}
+
+variable "website_index_document" {
+  type        = string
+  description = "Index Document for Website"
+  default     = "index.html"
+}
+
+variable "website_error_document" {
+  type        = string
+  description = "Error (Not Found) Document for Website"
+  default     = "404.html"
+}
+
+variable "website_cors_allowed_headers" {
+  type        = list
+  description = "Allowed Headers for Website CORS"
+  default     = ["*"]
+}
+
+variable "website_cors_allowed_methods" {
+  type        = list
+  description = "Allowed Methods for Website CORS"
+  default     = ["GET", "POST", "PUT"]
+}
+
+variable "website_cors_allowed_origin" {
+  type        = list
+  description = "Allowed Origins for Website CORS"
+  default     = ["https://hackthenortheast.com"]
+}
