2a99643fb7cd985dcd504de26581c51836c2a075
Kerim Satirli <k.satirli@gmail.com>
2020-06-05 21:36:52 +0200

    adds output for S3 Website Bucket

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@
 output "ssh_connection_string" {
   value = "ssh -i \"${var.ssh_key_file}\" ec2-user@${aws_instance.hack_the_ne.public_ip}"
 }
+
+output "website_url" {
+  value = aws_s3_bucket.hack_the_ne.website_endpoint
+}
